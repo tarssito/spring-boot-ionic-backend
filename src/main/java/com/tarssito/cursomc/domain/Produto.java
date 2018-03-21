1eb6c2efcbaf7b6507d1229e1cf5a71f80fd91fb
tarssito <tarssito@gmail.com>
2018-03-20 21:09:47 -0300

    Ajustes no endpoint /categorias/id

--- src/main/java/com/tarssito/cursomc/domain/Produto.java
+++ src/main/java/com/tarssito/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,11 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
+	/*
+	 * @JsonBackReference - omiti a lista categorias para cada produto
+	 * 
+	 */
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name="PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/tarssito/cursomc/services/CategoriaService.java
+++ src/main/java/com/tarssito/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.tarssito.cursomc.domain.Categoria;
 import com.tarssito.cursomc.repositories.CategoriaRepository;
+import com.tarssito.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
