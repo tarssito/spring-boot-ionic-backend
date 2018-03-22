b99be4b412b3c51e6af87d00c4aea0c0d063729b
tarssito <tarssito@gmail.com>
2018-03-22 16:27:42 -0300

    Validacao customizada: email nao repetido na atualização de Cliente

---
+++ src/main/java/com/tarssito/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.tarssito.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.tarssito.cursomc.domain.Cliente;
+import com.tarssito.cursomc.dto.ClienteDTO;
+import com.tarssito.cursomc.repositories.ClienteRepository;
+import com.tarssito.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repositoty;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>(); 
+		
+		Cliente cliente = repositoty.findByEmail(objDto.getEmail());
+		
+		if(cliente != null && !cliente.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
