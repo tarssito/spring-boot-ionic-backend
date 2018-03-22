5b8a9735daeac3366465e76d960901cb70231abb
tarssito <tarssito@gmail.com>
2018-03-21 22:52:05 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/tarssito/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tarssito/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.tarssito.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tarssito.cursomc.domain.Categoria;
+import com.tarssito.cursomc.dto.CategoriaDTO;
 import com.tarssito.cursomc.services.CategoriaService;
 
 @RestController
@@ -27,28 +30,34 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(categoria);
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
 		categoria = service.insert(categoria);
-		/* Retorna na url o id inserido (categorias/{id}*/
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(categoria.getId())
+		/* Retorna na url o id inserido (categorias/{id} */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id) {
 		categoria.setId(id);
 		categoria = service.update(categoria);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categorias = service.findAll();
+		List<CategoriaDTO> categoriasDTO = categorias.stream().map(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
+	}
+
 }
--- src/main/java/com/tarssito/cursomc/services/CategoriaService.java
+++ src/main/java/com/tarssito/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.tarssito.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,4 +43,9 @@ public class CategoriaService {
 		}
 
 	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+
 }
