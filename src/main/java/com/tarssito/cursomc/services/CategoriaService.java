509ab3122e01bdadb116f2fc94730739ed49bfd8
tarssito <tarssito@gmail.com>
2018-03-22 11:28:22 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/tarssito/cursomc/services/CategoriaService.java
+++ src/main/java/com/tarssito/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tarssito.cursomc.domain.Categoria;
+import com.tarssito.cursomc.dto.CategoriaDTO;
 import com.tarssito.cursomc.repositories.CategoriaRepository;
 import com.tarssito.cursomc.services.exceptions.DataIntegrityException;
 import com.tarssito.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 }
