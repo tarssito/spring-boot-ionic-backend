0979743d07fa9fc5ad741766b83f9fcf6e5dc900
tarssito <tarssito@gmail.com>
2018-03-22 10:28:29 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/tarssito/cursomc/services/CategoriaService.java
+++ src/main/java/com/tarssito/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tarssito.cursomc.domain.Categoria;
@@ -48,4 +51,8 @@ public class CategoriaService {
 		return repository.findAll();
 	}
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
 }
