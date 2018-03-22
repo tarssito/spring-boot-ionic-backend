695b81bf1c51239d2e886c8a995b107fc18512a0
tarssito <tarssito@gmail.com>
2018-03-21 22:19:13 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/tarssito/cursomc/services/CategoriaService.java
+++ src/main/java/com/tarssito/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -23,6 +23,11 @@ public class CategoriaService {
 
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
+		return repository.save(categoria);
+	}
+
+	public Categoria update(Categoria categoria) {
+		find(categoria.getId());
 		return repository.save(categoria);
 	}
 }
