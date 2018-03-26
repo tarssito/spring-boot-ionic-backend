a9ec6d578dc18d68db5dc97923d48f60f0f875a0
tarssito <tarssito@gmail.com>
2018-03-26 18:00:16 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/tarssito/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/tarssito/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,31 @@
 package com.tarssito.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tarssito.cursomc.domain.Categoria;
 import com.tarssito.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly=true) // desabilita transação
+	@Query("SELECT DISTINCT p FROM Produto p INNER JOIN p.categorias cat WHERE p.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
+	/**
+	 * Método que executa a mesma query do método acima, sem utilização da anotação @Query
+	 * @param nome
+	 * @param categorias
+	 * @param pageRequest
+	 * @return
+	 */
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/tarssito/cursomc/services/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.tarssito.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.tarssito.cursomc.domain.Categoria;
+import com.tarssito.cursomc.domain.Produto;
+import com.tarssito.cursomc.repositories.CategoriaRepository;
+import com.tarssito.cursomc.repositories.ProdutoRepository;
+import com.tarssito.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> produto = repository.findById(id);
+		return produto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.search(nome, categorias, pageRequest);
+
+	}
+
+}
