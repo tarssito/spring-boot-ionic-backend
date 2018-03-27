bcfc3da47adeaf5b4f0b52fc3f503bcbd92e4216
tarssito <tarssito@gmail.com>
2018-03-27 15:31:33 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/tarssito/cursomc/config/DevConfig.java
+++ src/main/java/com/tarssito/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.tarssito.cursomc.services.DBService;
+import com.tarssito.cursomc.services.EmailService;
+import com.tarssito.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,4 +30,9 @@ public class DevConfig {
 		dbService.instantiateTestDataBase();
 		return true;
 	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}	
 }
