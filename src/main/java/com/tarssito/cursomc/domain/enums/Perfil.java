5a7b79e0fa29e706bf0e492ba9b927e76c3a310c
tarssito <tarssito@gmail.com>
2018-03-29 15:43:07 -0300

    Atualizando endpoints para perfis especificos

---
+++ src/main/java/com/tarssito/cursomc/domain/enums/Perfil.java
@@ -0,0 +1,36 @@
+package com.tarssito.cursomc.domain.enums;
+
+public enum Perfil {
+
+	ADMIN(1, "ROLE_ADMIN"),
+	CLIENTE(2, "ROLE_CLIENTE");
+	
+	private int cod;
+	private String descricao;
+	
+	private Perfil(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Perfil toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		for (Perfil t : Perfil.values()) {
+			if (cod.equals(t.getCod())) {
+				return t;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+	
+}
