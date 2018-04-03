e932767bb8ddec4d177f06b2c1750abbfb6f94f3
tarssito <tarssito@gmail.com>
2018-04-02 22:03:37 -0300

    Enviando imagem via endpoint

--- src/main/java/com/tarssito/cursomc/resources/ClienteResource.java
+++ src/main/java/com/tarssito/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tarssito.cursomc.domain.Cliente;
@@ -80,6 +81,12 @@ public class ClienteResource {
 		Page<Cliente> clientes = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> clientesDTO = clientes.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(clientesDTO);
+	}
+	
+	@RequestMapping(value="/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/tarssito/cursomc/services/ClienteService.java
+++ src/main/java/com/tarssito/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.tarssito.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tarssito.cursomc.domain.Cidade;
 import com.tarssito.cursomc.domain.Cliente;
@@ -40,6 +42,9 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -115,4 +120,8 @@ public class ClienteService {
 		newCliente.setEmail(cliente.getEmail());
 	}
 
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
 }
